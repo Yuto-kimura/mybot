54c357304bc25d4443dc3caa5fccfa37a0018d33
mtmt <>
2020-02-05 13:39:07 +0900

    update: server system

--- py-server/main.py
+++ py-server/main.py
@@ -2,9 +2,9 @@ from flask import Flask, request, render_template, redirect, url_for, jsonify, a
 from datetime import datetime
 import ipaddress, os, sys, traceback, json
 from werkzeug.datastructures import ImmutableMultiDict
-import configparser
 
-from sql import exec_sql
+# from sql import exec_get_sql, exec_post_sql
+from sql import exec_post_sql
 from sql import SQLExceptionError
 # from api import exec_api
 # from api import APIExceptionError
@@ -16,33 +16,21 @@ app.config['JSON_AS_ASCII'] = False
 # config = configparser.ConfigParser()
 # config.read('config.ini')
 
-def get_request(keyword):
-    try:
-        response = exec_sql(keyword)
-        return response
-    except ValueError as e:
-        t, v, tb = sys.exc_info()
-        print(str(traceback.format_exception(t,v,tb)))
-        print(str(traceback.format_tb(e.__traceback__)))
-        abort(400, e)
+# def get_request(keyword):
+#     try:
+#         response = exec_get_sql(keyword)
+#         return response
+#     except ValueError as e:
+#         t, v, tb = sys.exc_info()
+#         print(str(traceback.format_exception(t,v,tb)))
+#         print(str(traceback.format_tb(e.__traceback__)))
+#         abort(400, e)
 
 ### >>> data = request.data(dict)
 def post_request(data):
     try:
-        # print(data, type(data))
-
-        if 'request' in data:
-            request_type = data['request']
-            pass
-
-        elif 'hash' in data:
-            pass
-
-        else:
-            raise ValueError("Unsuport search type.")
-
-        return None
-
+        response = exec_post_sql(data)
+        return response
     except ValueError as e:
         t, v, tb = sys.exc_info()
         print(str(traceback.format_exception(t,v,tb)))
@@ -77,27 +65,19 @@ def post_api_request():
     if request.method == 'POST':
         ### binary -> dict
         data = request.data.decode('utf-8')
-        data = post_request(json.loads(data))
-        return data
-    else:
-        abort(400, 'Unexpected request method.')
-
-@app.route('/yurubot/api/get/<keyword>', methods=['GET'])
-def get_api_request(keyword):
-    if request.method == 'GET':
-        res = get_request(keyword)
-        response = {}
-        response['type'] = 'GET'
-        result = []
-        for item in res:
-            tmp = {}
-            tmp['ID'] = item[0]
-            tmp['USER_NAME'] = item[1]
-            result.append(tmp)
-        response['data'] = result
+        response = post_request(json.loads(data))
         return response
     else:
         abort(400, 'Unexpected request method.')
+
+# @app.route('/yurubot/api/get/<keyword>', methods=['GET'])
+# def get_api_request(keyword):
+#     print("############# "+str(keyword))
+#     if request.method == 'GET':
+#         response = dict(get_request(keyword))
+#         return response
+#     else:
+#         abort(400, 'Unexpected request method.')
 
 def main():
     app.run()
--- py-server/sql.py
+++ py-server/sql.py
@@ -1,32 +1,28 @@
-import configparser
 import glob, json
 import sqlite3 as sql
 
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-dummy_record = [
-    {
-        'ID':'_231489509_-1463812096',
-        'USER_NAME':'kimura',
-        'KEYWORD':'daab'
-    },
-    {
-        'ID':'_231493067_1694498816',
-        'USER_NAME':'inoue',
-        'KEYWORD':'daab'
-    },
-    {
-        'ID':'_231489514_-1723858944',
-        'USER_NAME':'kiyasu',
-        'KEYWORD':'javascript'
-    },
-    {
-        'ID':'_231478384_251658240',
-        'USER_NAME':'matsumoto',
-        'KEYWORD':'nodejs'
-    }
-]
+# dummy_record = [
+#     {
+#         'ID':'_231489509_-1463812096',
+#         'USER_NAME':'kimura',
+#         'KEYWORD':'daab'
+#     },
+#     {
+#         'ID':'_231493067_1694498816',
+#         'USER_NAME':'inoue',
+#         'KEYWORD':'daab'
+#     },
+#     {
+#         'ID':'_231489514_-1723858944',
+#         'USER_NAME':'kiyasu',
+#         'KEYWORD':'javascript'
+#     },
+#     {
+#         'ID':'_231478384_251658240',
+#         'USER_NAME':'matsumoto',
+#         'KEYWORD':'nodejs'
+#     }
+# ]
 
 class SQLExceptionError(Exception):
     pass
@@ -36,10 +32,6 @@ class SearchRequests:
         self.database_path = "./yurubot_user.db"
         self.user_table = "yurubot_user"
         self.table_index = '("ID", "USER_NAME", "KEYWORD")'
-        # self.dbpath = config.get('database_path', 'database')
-        # self.datasets = datasets
-        # self.service_type = service_type
-        # self.input_value = str(input_value)
 
     def delete_table(self):
         conn=sql.connect(self.database_path)
@@ -67,111 +59,190 @@ class SearchRequests:
             conn.commit()
         conn.close()
 
-    # def request_service_count(self):
-    #     conn=sql.connect(self.dbpath)
-    #     cur=conn.cursor()
-    #     query = "SELECT count(*) FROM %s WHERE %s=?" % (self.datasets, self.service_type)
-    #     cur.execute(query, (self.input_value,))
-    #     results = cur.fetchall()
-    #     conn.commit()
-    #     conn.close()
-    #     return results
-    #
-    # def request_service_record(self, record):
-    #     conn=sql.connect(self.dbpath)
-    #     cur=conn.cursor()
-    #     query = "SELECT %s FROM %s WHERE %s=?" % (str(record), self.datasets, self.service_type)
-    #     cur.execute(query, (self.input_value,))
-    #     results = cur.fetchall()
-    #     conn.commit()
-    #     conn.close()
-    #     return results
+    ### 上　ダミーデータ
 
     def request_get_user(self, keyword):
-        conn=sql.connect(self.database_path)
-        cur=conn.cursor()
-        query = "SELECT %s, %s FROM %s where KEYWORD='%s'" % ('ID', 'USER_NAME', self.user_table, keyword)
-        cur.execute(query)
-        results = cur.fetchall()
-        conn.commit()
-        conn.close()
-        return results
+        try:
+            conn=sql.connect(self.database_path)
+            cur=conn.cursor()
+            query = "SELECT %s FROM %s where KEYWORD='%s'" % ('ID', self.user_table, keyword)
+            cur.execute(query)
+            results = cur.fetchall()
+            conn.commit()
+            conn.close()
+
+            # 重複(を除いた)検索結果
+            key_list = list(set(results))
+            result = []
+            for id in key_list:
+                result.append(id[0])
+
+            response = {
+                'code': 1,
+                'data': result
+            }
+        except Exception as e:
+            response = {
+                'code': 0,
+                'data': []
+            }
+        finally:
+            return response
 
     def request_check_user(self, record):
-        pass
+        try:
+            conn=sql.connect(self.database_path)
+            cur=conn.cursor()
+            query = "SELECT %s FROM %s where ID='%s'" % ('KEYWORD', self.user_table, record['ID'])
+            cur.execute(query)
+            results = cur.fetchall()
+            conn.commit()
+            conn.close()
 
-    def request_edit_user(self, record):
-        pass
+            # 重複(を除いた)検索結果
+            key_list = list(set(results))
+            result = []
+            for item in key_list:
+                result.append(item[0])
+
+            response = {
+                'code': 1,
+                'data': result
+            }
+        except Exception as e:
+            response = {
+                'code': 0,
+                'data': []
+            }
+        finally:
+            return response
+
+
+    def request_update_user(self, record):
+        try:
+            for index in range(len(record['KEYWORD'])):
+                conn=sql.connect(self.database_path)
+                cur=conn.cursor()
+                query = "INSERT INTO %s (%s, %s, %s) VALUES ('%s', '%s', '%s')" % (self.user_table, 'ID', 'USER_NAME', 'KEYWORD', str(record['ID']), record['USER_NAME'], record['KEYWORD'][index])
+                print(query)
+                cur.execute(query)
+                results = cur.fetchall()
+                conn.commit()
+            conn.close()
+
+            response = {
+                'code': 1,
+                'data': 1
+            }
+        except Exception as e:
+            response = {
+                'code': 0,
+                'data': 0
+            }
+        finally:
+            return response
+
 
     def request_delete_user(self, record):
-        pass
+        try:
+            for index in range(len(record['KEYWORD'])):
+                conn=sql.connect(self.database_path)
+                cur=conn.cursor()
+                query = "DELETE FROM %s WHERE ID='%s' AND KEYWORD='%s'" % (self.user_table, record['ID'], record['KEYWORD'][index])
+                cur.execute(query)
+                results = cur.fetchall()
+                conn.commit()
+            conn.close()
+
+            response = {
+                'code': 1,
+                'data': 1
+            }
+        except Exception as e:
+            response = {
+                'code': 0,
+                'data': 0
+            }
+        finally:
+            return response
 
     def request_regist_user(self, record):
+        try:
+            for index in range(len(record['KEYWORD'])):
+                conn=sql.connect(self.database_path)
+                cur=conn.cursor()
+                query = "INSERT INTO %s (%s, %s, %s) VALUES ('%s', '%s', '%s')" % (self.user_table, 'ID', 'USER_NAME', 'KEYWORD', record['ID'], record['USER_NAME'], record['KEYWORD'][index])
+                cur.execute(query)
+                results = cur.fetchall()
+                conn.commit()
+            conn.close()
+
+            response = {
+                'code': 1,
+                'data': 1
+            }
+        except Exception as e:
+            response = {
+                'code': 0,
+                'data': 0
+            }
+        finally:
+            return response
+
+
+    def check(self):
         conn=sql.connect(self.database_path)
         cur=conn.cursor()
 
-        query = "INSERT INTO %s (%s, %s, %s) VALUES (%s, %s, %s)" % (self.user_table, 'ID', 'USER_NAME', 'KEYWORD', record['id'], record['user_name'], record['keyword'])
-        # query = "SELECT %s FROM %s WHERE %s=?" % (str(record), self.datasets, self.service_type)
-        # query = "SELECT %s FROM %s WHERE %s=?" % (self.user_table, str(columun), self.service_type)
-        cur.execute(query, (self.input_value,))
+        query = "SELECT * FROM %s" % (self.user_table,)
+        cur.execute(query)
 
         results = cur.fetchall()
         conn.commit()
         conn.close()
 
-        return results
-
-# def get_db_config(datasets):
-#     table_index_list = []
-#     conf = config.get('datasets_db_conf', 'datasets_conf')
-#     try:
-#         with open(conf, 'r') as rf:
-#             db_config = json.load(rf)
-#             for dataset_name in db_config:
-#                 if dataset_name['datasets'] == datasets:
-#                     return dataset_name['style_table_index'], dataset_name['field_name_label']
-#
-#         raise SQLExceptionError([500, "Unexpected error."])
-#     except Exception as e:
-#         raise SQLExceptionError([500, str(e)])
-
-# def load_table_lable(datasets):
-#     table_index, field_name = get_db_config(datasets)
-#     field_label = []
-#     field_index = ""
-#     for field in table_index:
-#         index = field.split(' ')[0]
-#         if index == "id":
-#             continue
-#         field_index = field_index + index + ", "
-#         field_label.append(field_name[index])
-#     return field_index[0:-2], field_label
-
-# def perse_request(req_search):
-#     try:
-#         data = []
-#         count = req_search.request_service_count()
-#
-#         data.append({"件数":str(count[0][0])})
-#         if count[0][0] > 0:
-#             record, field_label = load_table_lable(req_search.datasets)
-#             result = req_search.request_service_record(record)
-#             for num in range(0, len(result[0])):
-#                 data.append({str(field_label[num]):str(result[0][num])})
-#
-#         output = {'service': req_search.datasets, 'data': data}
-#         return output
-#     except Exception as e:
-#         raise SQLExceptionError([500, str(e)])
+        print("######### table status")
+        # 重複(を除いた)検索結果
+        # result = list(set(results))
+        result = results
+        for item in result:
+            print(item)
+        print("######### #########")
 
 
-def exec_sql(keyword):
-    ### class __init__
-    req_search = SearchRequests()
+def create_dummy(req_search):
     ### dummy データベースセット
     req_search.delete_table()
     req_search.creat_table()
-    req_search.setup_dummy_table()
-    ###
-    response_data = req_search.request_get_user(keyword)
+    ### dummy レコードの追加
+    # req_search.setup_dummy_table()
+
+def exec_post_sql(record):
+    req_search = SearchRequests()
+    # create_dummy(req_search) ### dummy データベースセット
+    if record['REQUEST'] == "NEWUSER":
+        response_data = req_search.request_regist_user(record)
+    elif record['REQUEST'] == "DELETE":
+        response_data = req_search.request_delete_user(record)
+    elif record['REQUEST'] == "ADD":
+        response_data = req_search.request_update_user(record)
+    elif record['REQUEST'] == "CHECK":
+        response_data = req_search.request_check_user(record)
+    elif record['REQUEST'] == "GET":
+        response_data = req_search.request_get_user(record['KEYWORD'])
+    elif record['REQUEST'] == "CLEAR":
+        create_dummy(req_search)
+        response_data = {
+            'code': 1,
+            'data': 1
+        }
+    else:
+        # raise SQLExceptionError([500, "Unexpected error."])
+        response_data = {
+            'code': 0,
+            'data': []
+        }
+    print(response_data)
+
+    req_search.check()
     return response_data
